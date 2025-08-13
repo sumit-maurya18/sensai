5c71957b23d5605576a223994f67a35ada659384
sumit-maurya18 <techaid185@gmail.com>
2025-08-13 23:24:44 +0530

    Added header layout and fixed Clerk middleware bug and created some protection through routes to prevent unauthoried access

--- components/header.jsx
+++ components/header.jsx
@@ -1,8 +1,92 @@
+import { SignInButton, UserButton, SignedIn, SignedOut } from '@clerk/nextjs';
 import React from 'react'
+import Link from 'next/link';
+import Image from 'next/image';
+import { ChevronDown, FileText, GraduationCap, LayoutDashboard, PenBox, StarsIcon } from 'lucide-react';
+import { Button } from './ui/button';
+import { DropdownMenu, DropdownMenuTrigger, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator } from './ui/dropdown-menu';
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <header className="fixed top-0 w-full border-b bg-background/80 backdrop-blur-md z-50 supports-[backdrop-filter]:bg-background/60">
+
+      <nav className="container mx-auto px-4 h-16 flex items-center justify-between">
+        <Link href="/">
+          <Image src="/logo.png" alt="SensAI Logo" width={200} height={60} className="h-12 py-1 w-auto object-contain" />
+        </Link>
+
+        <div className='flex items-center space-x-2 md:space-x-4'>
+
+          <SignedIn>
+            <Link href={"/dashboard"}>
+              <Button variant='outline'>
+                <LayoutDashboard className='h-4 w-4' />
+                <span className='hidden md:block'>Industry Insights</span>
+              </Button>
+            </Link>
+
+
+            <DropdownMenu>
+
+              <DropdownMenuTrigger asChild>
+                <Button>
+                  <StarsIcon className='h-4 w-4' />
+                  <span className='hidden md:block'>Growth Tools</span>
+                  <ChevronDown className='h-4 w-4' />
+                </Button>
+              </DropdownMenuTrigger>
+
+              <DropdownMenuContent>
+
+                <DropdownMenuItem>
+                  <Link href={"/resume"} className='flex items-center gap-2'>
+                    <FileText className='h-4 w-4' />
+                    <span>Build Resume</span>
+                  </Link>
+                </DropdownMenuItem>
+
+                <DropdownMenuItem>
+                  <Link href={"/ai-cover-letter"} className='flex items-center gap-2'>
+                    <PenBox className='h-4 w-4' />
+                    <span>Cover Letter</span>
+                  </Link>
+                </DropdownMenuItem>
+
+                <DropdownMenuItem>
+                  <Link href={"/interview-page"} className='flex items-center gap-2'>
+                    <GraduationCap className='h-4 w-4' />
+                    <span>Interview Prep</span>
+                  </Link>
+                </DropdownMenuItem>
+
+              </DropdownMenuContent>
+            </DropdownMenu>
+
+          </SignedIn>
+
+          <SignedOut>
+            <SignInButton>
+              <Button variant="outline">Sign In</Button>
+            </SignInButton>
+          </SignedOut>
+
+          <SignedIn>
+            <UserButton appearance={{
+              elements:{
+                avatarBox:'w-10 h-10',
+                userButtonPopoverCard:'shadow-xl',
+                userPreviewMainIdentifier:'font-semibold'
+              }
+            }}
+            afterSignOutUrl='/'
+            />
+          </SignedIn>
+
+        </div>
+      </nav>
+
+
+    </header>
   )
 }
 
